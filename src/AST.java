4035bf7b21c0c4d65ad36cdffc11b96c469d6e55
AKINCI <ozgurakinci@outlook.com>
2023-03-24 19:37:20 +0300

    Int Parser

--- src/AST.java
+++ src/AST.java
@@ -33,17 +33,17 @@ class PolynomialFunction extends Exp {
         StringBuilder poly = new StringBuilder();
         if(type.equals(AppConstant.PolynomialFunction)) {
             // p(x) = cnx^n + ... + c3x^3 + c2x^2 + c1x + c0
-            for (int n = size-1 ; n > 0; n--) {
+            for (int n = size-1 ; n >= 0; n--) {
                 poly.append("x^").append(n);
-                if(n != 1) {
+                if(n != 0) {
                     poly.append("+");
                 }
             }
         }else if (type.equals(AppConstant.PolynomialIntFunction)) {
             // P(x) = (cnx^(n+1) / (n+1)) + .... + ((c3x^4) / 4) + ((c2x^3) / 3) + ((c1x^2) / 2) + (c0x)
-            for (int n = size-1 ; n > 0; n--) {
+            for (int n = size-1 ; n >= 0; n--) {
                 poly.append("(").append("(").append(n).append("x^").append(n+1).append(")").append("/").append(n+1).append(")");
-                if(n != 1) {
+                if(n != 0) {
                     poly.append("+");
                 }
             }
