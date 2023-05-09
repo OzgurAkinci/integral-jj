9c4806d842de8fc17da4b8873f4edcccd7aae944
AKINCI <ozgurakinci@outlook.com>
2023-05-09 11:15:51 +0300

    Int Parser

--- src/EvalVisitor.java
+++ src/EvalVisitor.java
@@ -10,18 +10,18 @@ class EvalVisitor implements Visitor {
 
     public Object visit(INT e) {
         // input
-        System.out.println("========================================================");
+        System.out.println("========================= input(n) ======================================");
         n = ((Num) e.exp).number;
         System.out.println("n = " + n);
 
         // pointers
-        System.out.println("========================================================");
+        System.out.println("========================= h & y =========================================");
         PointerDTO[] pointers = AppConstant.calcPointers(n+1);
         System.out.println("h = " + Arrays.stream(pointers).toList().stream().map(PointerDTO::gethCoefficient).collect(Collectors.joining(",")));
         System.out.println("y = " + Arrays.stream(pointers).toList().stream().map(PointerDTO::getyCoefficient).collect(Collectors.joining(",")));
 
         // poly
-        System.out.println("========================================================");
+        System.out.println("========================= Polynomial Function ===========================");
         PolynomialDTO polynomialDTO = AppConstant.getPolynomialDto(n);
         System.out.println(AppConstant.PolynomialFunction + " = "+ polynomialDTO.getPoly());
         System.out.println(AppConstant.PolynomialIntFunction + " = "+ polynomialDTO.getPolyInt());
